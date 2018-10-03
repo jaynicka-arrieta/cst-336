51562ba4c9e68e39c0480560811bdbff5301f55d
Sam Laitha <slaitha@csumb.edu>
2018-10-03 21:33:07 +0000

    css

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Team 2: Silver Jack </title>
+        
+        <h1><center><img src = "img/poke_logo.png" alt = "poke logo" title = "poke logo" width = "500px"/></center></h1>
+        <!--<h1><center><img src = "img/jack.png" alt = "jack logo" title = "jack logo" width = "150px"/></center></h1>-->
+        
+    </head>
+    <style>
+        h1 {
+            border-bottom:10px;
+            text-decoration:none;
+            color:#42bff4;
+        }
+        
+        body {
+            background-image: url("img/back.jpg"); /* new tag */
+            background-size: 100%;
+            background-repeat: no-repeat;
+            text-align: center;
+        }
+        
+        hr{
+            text-align: center;
+            /*margin: 2%;*/
+            border-style: inset;
+            border-width: 4px;
+            width: 50%;
+            
+        }
+        div {
+            text-align: center;
+            /*background-color: gray;*/
+            /*opacity: 0.5;*/
+            /*filter: alpha(opacity=50);*/
+            
+            width: 50%;
+            
+        }
+        
+        
+    </style>
+    
+    <body>
+        
+        <div>
+            Player 1
+        </div>
+        <hr>
+        <!--div 1 : Player 1-->
+        
+        <div>
+            Player 1
+        </div>
+        <hr>
+        <!--div 2 : Player 2-->
+        
+        <div> 
+            Player 1
+        </div>
+        <hr>
+        <!--div 3 : Player 3-->
+        
+        <div> 
+            Player 1
+        </div>
+        <hr>
+        <!--div 4 : Player 4-->
+        
+        <div> 
+            Player[] Won! Total Points: 
+        </div>
+        <hr>
+        <!--div 5 display who won-->
+        
+        
+        
+        
+    </body>
+</html>
